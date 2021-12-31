1c1b2902ca07021b307b228eb39c24a7d0820311
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2021-12-30 21:35:15 -0500

    basic build

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { useReducer} from 'react';
+
+import './App.css';
+
+import TotalDisplay from './components/TotalDisplay';
+import CalcButton from './components/CalcButton';
+
+import reducer, { initialState } from './reducers';
+import { addOne, applyNumber, changeOperation, clearDisplay, memoryIsTotal, memoryApplied, memoryClear } from './actions';
+
+function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div className="App">
+      <nav className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">Calculator with React useReducer hook</a>
+      </nav>
+
+      <div className = "container row mt-5">
+        <div className="col-md-12 d-flex justify-content-center">
+          <form name="Cal">
+            
+            <TotalDisplay value={state.total}/>
+            <div className="row details">
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
+            </div>
+            
+            <div className="row">
+              <CalcButton value={"M+"} onClick={()=> dispatch(memoryIsTotal())} />
+              <CalcButton value={"MR"} onClick={()=> dispatch(memoryApplied())} />
+              <CalcButton value={"MC"} onClick={()=> dispatch(memoryClear())} />
+            </div>
+
+            <div className="row">
+              <CalcButton value={1} onClick={()=> dispatch(applyNumber(1))} />
+              <CalcButton value={2} onClick={()=> dispatch(applyNumber(2))} />
+              <CalcButton value={3} onClick={()=> dispatch(applyNumber(3))} />
+            </div>
+
+            <div className="row">
+              <CalcButton value={4} onClick={()=> dispatch(applyNumber(4))} />
+              <CalcButton value={5} onClick={()=> dispatch(applyNumber(5))} />
+              <CalcButton value={6} onClick={()=> dispatch(applyNumber(6))} />
+            </div>
+
+            <div className="row">
+              <CalcButton value={7} onClick={()=> dispatch(applyNumber(7))} />
+              <CalcButton value={8} onClick={()=> dispatch(applyNumber(8))} />
+              <CalcButton value={9} onClick={()=> dispatch(applyNumber(9))} />
+            </div>
+
+            <div className="row">
+              <CalcButton value={"+"} onClick={()=> dispatch(changeOperation('+'))} />
+              <CalcButton value={"*"} onClick={()=> dispatch(changeOperation('*'))} />
+              <CalcButton value={"-"} onClick={()=> dispatch(changeOperation('-'))} />
+            </div>
+
+            <div className="row ce_button">
+              <CalcButton value={"CE"} onClick={()=> dispatch(clearDisplay())} />
+            </div>
+
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,69 @@
+import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION, CLEAR_DISPLAY, MEMORY_IS_TOTAL, MEMORY_APPLIED, MEMORY_CLEAR, memoryClear } from './../actions';
+
+export const initialState = {
+    total: 0,
+    operation: "+",
+    memory: 0
+}
+
+const calculateResult = (num1, num2, operation) => {
+    switch(operation) {
+        case("+"):
+            return num1 + num2;
+        case("*"):
+            return num1 * num2;
+        case("-"):
+            return num1 - num2;
+    }
+}
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case(ADD_ONE):
+            return({
+                ...state,
+                total: state.total + 1
+            });
+
+        case(APPLY_NUMBER):
+            return ({ 
+                ...state, 
+                total: calculateResult(state.total, action.payload, state.operation)
+            });
+        
+        case(CHANGE_OPERATION):
+            return ({
+                ...state,
+                operation: action.payload
+            });
+
+        case(CLEAR_DISPLAY):
+            return({
+                ...state,
+                total: 0
+            });
+
+        case(MEMORY_IS_TOTAL):
+            return({
+                ...state,
+                memory: state.total
+            });
+
+        case(MEMORY_APPLIED):
+            return({
+                ...state,
+                total: calculateResult(state.total, state.memory, state.operation)
+            });
+
+        case(MEMORY_CLEAR):
+            return({
+                ...state,
+                memory: 0
+            });
+            
+        default:
+            return state;
+    }
+}
+
+export default reducer;
